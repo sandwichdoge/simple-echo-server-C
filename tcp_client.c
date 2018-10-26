10d8efafb923e7692398b5993c7e8a05af8cb7c8
sandwichdoge <38368906+sandwichdoge@users.noreply.github.com>
2018-10-26 15:40:02 +0700

    handle errors + comment clarity

--- tcp_client.c
+++ tcp_client.c
@@ -17,7 +17,10 @@ int main()
 {
     char incoming[2048];
     char outgoing[1024];
-    int client_fd = create_client_sock("localhost", 12345);
+    
+    int client_fd = create_client_sock("localhost", 12345); //create a client connection socket to target server
+    if (client_fd <0) return -1; //failed to create client socket
+    
     read(client_fd, incoming, sizeof(incoming)); //get welcome message from server
     printf("%s\n", incoming);
 
@@ -35,15 +38,16 @@ int main()
 int create_client_sock(char *hostname, int port)
 {
     struct hostent *he;
-    he = gethostbyname(hostname);
-    if (he == NULL) {; //query DNS for IP address
+    he = gethostbyname(hostname); //query DNS for IP address
+    if (he == NULL) {
         printf("Could not resolve hostname.\n");
+        return -1;
     }
     
     struct sockaddr_in server;
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = *(in_addr_t*)he->h_addr_list[0]; //s_addr is black magic
-    server.sin_port = htons(port);
+    server.sin_family = AF_INET; //INET connection
+    server.sin_addr.s_addr = *(in_addr_t*)he->h_addr_list[0]; //sin_addr is black magic, it's different a hack on each system
+    server.sin_port = htons(port); //convert to compliant network short int
 
     int client_fd = socket(AF_INET, SOCK_STREAM, 0);
     int err = connect(client_fd, (struct sockaddr*)&server, sizeof(server));
